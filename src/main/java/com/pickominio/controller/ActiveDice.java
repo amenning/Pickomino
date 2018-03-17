55a392e37d0c037b1946fa32a040d8f86ea29135
Andrew Menning <menning23@gmail.com>
2018-03-16 22:22:34 -0400

    dynamically load starting active dice

--- src/main/java/com/pickominio/controller/ActiveDice.java
+++ src/main/java/com/pickominio/controller/ActiveDice.java
@@ -1,8 +1,27 @@
 package com.pickominio.controller;
 
+import com.pickominio.model.DiceSet;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
 
 public class ActiveDice {
+    @FXML
+    private HBox activeDiceBox;
+
+    public void initialize() throws IOException {
+        for (int count = 0; count < DiceSet.MAX_DICE_SET_SIZE; ++count) {
+            StackPane dice = FXMLLoader.load(
+                getClass().getResource("/fxml/dice.fxml")
+            );
+            activeDiceBox.getChildren().add(dice);
+        }
+    }
+
     public void freezeDice(ActionEvent actionEvent) {
         System.out.println("Test Dice");
     }
